a500f6f92e14f61fbae009b017146b037bebb263
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-05-27 10:01:28 +0000

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
     return (
         <>
@@ -33,6 +33,6 @@ function App() {
             </p>
         </>
     )
-}
+};
 
-export default App
+export default App;
