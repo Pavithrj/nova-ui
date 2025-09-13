a11f080cc7d1679a3c4a6e4dc764052c8988bdb2
Pavithr Jain <pavithrjain19@gmail.com>
2025-09-13 22:38:39 +0530

    111

---
+++ src/components/FaqItem.jsx
@@ -0,0 +1,17 @@
+
+const FaqItem = ({ item, index }) => {
+    return (
+        <div className="relative mb-12 z-2">
+            <div className="">
+                <div className="flex-1">
+
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default FaqItem;
+
+
+// 2:03:30
