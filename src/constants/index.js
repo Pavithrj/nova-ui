eac62bec3e5f7c132e93c46d24095c6d71318554
Pavithr Jain <pavithrjain19@gmail.com>
2025-09-13 22:37:50 +0530

    109

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,116 +46,116 @@ export const details = [
     },
 ];
 
-// export const faq = [
-//     {
-//         id: '0',
-//         question: 'How easy is it to setup Xora?',
-//         answer:
-//             'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
-//     },
-//     {
-//         id: '1',
-//         question: 'Can I integrate Xora with other platforms?',
-//         answer:
-//             'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
-//     },
-//     {
-//         id: '2',
-//         question: 'How often do you add new content?',
-//         answer:
-//             'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
-//     },
-//     {
-//         id: '3',
-//         question: 'What your refund policy?',
-//         answer:
-//             'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
-//     },
-//     {
-//         id: '4',
-//         question: 'Do you have corporate plans?',
-//         answer:
-//             'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
-//     },
-//     {
-//         id: '5',
-//         question: 'What happens when I’m out of storage?',
-//         answer:
-//             'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
-//     },
-//     {
-//         id: '6',
-//         question: 'Can I upgrade my plan?',
-//         answer:
-//             'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
-//     },
-//     {
-//         id: '7',
-//         question: 'How do I invite my team?',
-//         answer:
-//             'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
-//     },
-//     {
-//         id: '8',
-//         question: 'Do you offer training for individuals and teams?',
-//         answer:
-//             'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
-//     },
-//     {
-//         id: '9',
-//         question: 'I’m locked out of my account what do I do?',
-//         answer:
-//             'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
-//     },
-// ];
+export const faq = [
+    {
+        id: '0',
+        question: 'How easy is it to setup Xora?',
+        answer:
+            'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
+    },
+    {
+        id: '1',
+        question: 'Can I integrate Xora with other platforms?',
+        answer:
+            'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
+    },
+    {
+        id: '2',
+        question: 'How often do you add new content?',
+        answer:
+            'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
+    },
+    {
+        id: '3',
+        question: 'What your refund policy?',
+        answer:
+            'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
+    },
+    {
+        id: '4',
+        question: 'Do you have corporate plans?',
+        answer:
+            'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
+    },
+    {
+        id: '5',
+        question: 'What happens when I’m out of storage?',
+        answer:
+            'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
+    },
+    {
+        id: '6',
+        question: 'Can I upgrade my plan?',
+        answer:
+            'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
+    },
+    {
+        id: '7',
+        question: 'How do I invite my team?',
+        answer:
+            'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
+    },
+    {
+        id: '8',
+        question: 'Do you offer training for individuals and teams?',
+        answer:
+            'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
+    },
+    {
+        id: '9',
+        question: 'I’m locked out of my account what do I do?',
+        answer:
+            'Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.',
+    },
+];
 
-// export const plans = [
-//     {
-//         id: '0',
-//         title: 'Core',
-//         priceMonthly: 19,
-//         priceYearly: 12,
-//         caption: 'Best for solo creators',
-//         features: [
-//             '100MB Cloud storage',
-//             '100+ prompt templates',
-//             '5 projects',
-//             '24/7 support',
-//         ],
-//         icon: '/images/circle.svg',
-//         logo: '/images/plan-1.png',
-//     },
-//     {
-//         id: '1',
-//         title: 'Overdrive',
-//         priceMonthly: 79,
-//         priceYearly: 59,
-//         caption: 'Most popular plan',
-//         features: [
-//             'All Starter features',
-//             '1TB additional storage',
-//             'Unlimited projects',
-//             'Analytics',
-//         ],
-//         icon: '/images/triangle.svg',
-//         logo: '/images/plan-2.png',
-//     },
-//     {
-//         id: '2',
-//         title: 'Team',
-//         priceMonthly: 39,
-//         priceYearly: 29,
-//         caption: 'Exclusively for teams',
-//         features: [
-//             'All Overdrive features',
-//             '10TB additional storage',
-//             '50% off per member',
-//             'Real-time collaboration',
-//         ],
-//         icon: '/images/hexagon.svg',
-//         logo: '/images/plan-3.png',
-//     },
-// ];
+export const plans = [
+    {
+        id: '0',
+        title: 'Core',
+        priceMonthly: 19,
+        priceYearly: 12,
+        caption: 'Best for solo creators',
+        features: [
+            '100MB Cloud storage',
+            '100+ prompt templates',
+            '5 projects',
+            '24/7 support',
+        ],
+        icon: 'nova-ui/src/assets/images/circle.svg',
+        logo: 'nova-ui/src/assets/images/plan-1.png',
+    },
+    {
+        id: '1',
+        title: 'Overdrive',
+        priceMonthly: 79,
+        priceYearly: 59,
+        caption: 'Most popular plan',
+        features: [
+            'All Starter features',
+            '1TB additional storage',
+            'Unlimited projects',
+            'Analytics',
+        ],
+        icon: 'nova-ui/src/assets/images/triangle.svg',
+        logo: 'nova-ui/src/assets/images/plan-2.png',
+    },
+    {
+        id: '2',
+        title: 'Team',
+        priceMonthly: 39,
+        priceYearly: 29,
+        caption: 'Exclusively for teams',
+        features: [
+            'All Overdrive features',
+            '10TB additional storage',
+            '50% off per member',
+            'Real-time collaboration',
+        ],
+        icon: 'nova-ui/src/assets/images/hexagon.svg',
+        logo: 'nova-ui/src/assets/images/plan-3.png',
+    },
+];
 
 // export const testimonials = [
 //     {
